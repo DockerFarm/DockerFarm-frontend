686d9423d1d9029c2aabe7e54183295417ee8671
HoSung Lim <holnet1026@gmail.com>
2018-07-30 16:29:18 +0900

    feat(structure): default index file added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Root from 'containers/Root'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Root/>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
