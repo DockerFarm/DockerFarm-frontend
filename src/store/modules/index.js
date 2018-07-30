9cb7585ba66680612924f9187dfe85116150fb51
HoSung Lim <holnet1026@gmail.com>
2018-07-30 23:06:43 +0900

    feat(reducer): redux-form reducer added

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { penderReducer } from 'redux-pender';
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
 
+    form: formReducer,
     pender: penderReducer
 });
