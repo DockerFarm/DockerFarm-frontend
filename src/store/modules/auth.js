64d98ba761465a38964dee03b8acbd8c0ece5889
y0c <wonder789@naver.com>
2018-07-31 23:54:23 +0900

    feat(auth): signup api & reducer added

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -4,17 +4,29 @@ import { pender } from 'redux-pender';
 import { Map, fromJS } from 'immutable';
 
 const LOCAL_LOGIN = 'auth/LOCAL_LOGIN';
+const LOCAL_SIGNUP = 'auth/LOCAL_SIGNUP';
+const INIT = 'auth/INIT';
 
+export const init = createAction(INIT);
 export const login = createAction(LOCAL_LOGIN, AuthApi.login);
+export const signup = createAction(LOCAL_SIGNUP, AuthApi.signup);
 
 const initialState = Map({
     error : ''
 });
 
 export default handleActions({
-
+    [INIT]: (state,action) =>{
+        return state.set('error', null);
+    },
     ...pender({
         type: LOCAL_LOGIN,
+        onFailure(state, action) {
+            return state.set('error', fromJS(action.payload.response.data));
+        }
+    }),
+    ...pender({
+        type: LOCAL_SIGNUP,
         onFailure(state, action) {
             return state.set('error', fromJS(action.payload.response.data));
         }
