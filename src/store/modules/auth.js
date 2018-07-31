f0af4501a41a47731c64fb79e9d70d12dc7f6cc8
y0c <wonder789@naver.com>
2018-07-31 21:50:09 +0900

    feat(auth): implement auth reducer

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,13 +1,23 @@
-import { AuthAPI } from 'lib/api';
-import { createAction, handleActions } from 'redux-action';
+import { AuthApi } from 'lib/api';
+import { createAction, handleActions } from 'redux-actions';
 import { pender } from 'redux-pender';
+import { Map, fromJS } from 'immutable';
 
-const LOGIN = 'auth/LOGIN';
+const LOCAL_LOGIN = 'auth/LOCAL_LOGIN';
 
-export const login = createAction(LOGIN, AuthAPI.login);
+export const login = createAction(LOCAL_LOGIN, AuthApi.login);
 
 const initialState = Map({
-    form : {
+    error : ''
+});
 
-    }
-})
+export default handleActions({
+
+    ...pender({
+        type: LOCAL_LOGIN,
+        onFailure(state, action) {
+            return state.set('error', fromJS(action.payload.response.data));
+        }
+    })
+    
+}, initialState);
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import { penderReducer } from 'redux-pender';
 import { reducer as formReducer } from 'redux-form/immutable';
+import auth from './auth';
 
 export default combineReducers({
-
+    auth,
     form: formReducer,
     pender: penderReducer
 });
