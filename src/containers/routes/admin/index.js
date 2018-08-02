86e9a2aeea136bd98f429fd7076bdb7d9112bdcb
HoSung Lim <holnet1026@gmail.com>
2018-08-02 19:16:23 +0900

    feat(route): admin default page added

---
+++ src/containers/routes/admin/index.js
@@ -0,0 +1,9 @@
+import SettingPage from './SettingPage';
+import ImagePage from './ImagePage';
+import ContainerPage from './ContainerPage';
+
+export {
+    SettingPage,
+    ContainerPage,
+    ImagePage
+};
