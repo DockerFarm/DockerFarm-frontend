50a23ddeaad0487de95f82f8bfd43bfcf39d2531
y0c <wonder789@naver.com>
2018-08-03 23:35:42 +0900

    wip(spinner): http request loaders added

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,7 @@ import Routes from './routes';
 import * as user from 'store/modules/user';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
+import { Spinner } from 'components/base/ui';
 // import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
@@ -21,9 +22,12 @@ class App extends Component {
     }
 
     render() {
+        // const { pending } = this.props;
+        // const isLoading = Object.keys(pending).filter( k => pending[k] ).length;
         return (
             <div>
                 <Routes/>
+                {/* <Spinner visible={isLoading}/> */}
             </div>
         );
     }
@@ -33,6 +37,9 @@ export default compose(
     withRouter,
     connect( 
         null,
+        // state => ({
+        //     pending: state.pender.pending
+        // }),
         dispatch => ({ 
             UserAction: bindActionCreators(user, dispatch)
         }
