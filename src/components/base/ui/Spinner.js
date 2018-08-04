0b94d799c1f61efa8a5f613d5d565422d00aba65
HoSung Lim <holnet1026@gmail.com>
2018-08-04 23:30:08 +0900

    feat(ui): implement ajax loader via spinner component

--- src/components/base/ui/Spinner.js
+++ src/components/base/ui/Spinner.js
@@ -53,7 +53,7 @@ const SpinnerWrap = styled.div`
 
 const Spinner = ({ visible }) => (
     <SpinnerWrap visible={visible}>
-        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
     </SpinnerWrap>
 );
 
