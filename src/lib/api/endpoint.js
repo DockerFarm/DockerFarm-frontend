3ade0ffedd8f16c52904441aa6539fdeea1f458a
y0c <wonder789@naver.com>
2018-08-05 23:08:23 +0900

    refactor(endpoint): endpoint api url changed

--- src/lib/api/endpoint.js
+++ src/lib/api/endpoint.js
@@ -1,5 +1,6 @@
 import http from 'lib/httpClient';
 
-export const selectAllEndpoint = () => http.get('/user/endpoint');
-export const addEndpoint = form => http.post('/user/endpoint', form);
-export const removeEndpoint = id => http.delete(`/user/endpoint/${id}`);
+export const selectAllEndpoint = () => http.get('/admin/endpoint');
+export const addEndpoint = form => http.post('/admin/endpoint', form);
+export const removeEndpoint = id => http.delete(`/admin/endpoint/${id}`);
+export const updateEndpoint = (id,form) => http.put(`/admin/endpoint/${id}`, form);
