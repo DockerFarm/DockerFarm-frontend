5c6323c79d467f7dbb3cc151dfd5771afcfd9cb7
y0c <wonder789@naver.com>
2018-08-05 23:07:12 +0900

    feat(ui): common ui component added

---
+++ src/components/base/ui/LinkTitle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LinkTitle = styled.a`
+    cursor:pointer;
+    text-decoration:underline;
+    &:hover{
+        text-decoration:underline;
+    }
+`
+
+
+export default LinkTitle;
--- src/components/base/ui/index.js
+++ src/components/base/ui/index.js
@@ -1,7 +1,13 @@
 import Logo from './Logo';
 import Spinner from './Spinner';
+import TableNoData from './TableNoData';
+import TableColGroup from './TableColGroup';
+import LinkTitle from './LinkTitle';
 
 export {
     Logo,
-    Spinner
+    Spinner,
+    TableNoData,
+    TableColGroup,
+    LinkTitle
 }
