a733f3300a41a9bab5174c73e4962bed1349c46b
y0c <wonder789@naver.com>
2018-08-06 23:30:47 +0900

    feat(container): container api & reducer added

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -4,11 +4,13 @@ import { reducer as formReducer } from 'redux-form/immutable';
 import auth from './auth';
 import user from './user';
 import endPoint from './endpoint';
+import container from './container';
 
 export default combineReducers({
     auth,
     user,
     endPoint,
+    container,
     form: formReducer,
     pender: penderReducer
 });
