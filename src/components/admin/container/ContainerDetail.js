4022855cee232f0ee48aa75b0297e001a7015db8
y0c <wonder789@naver.com>
2018-08-08 23:43:43 +0900

    fix(component): key prop added

--- src/components/admin/container/ContainerDetail.js
+++ src/components/admin/container/ContainerDetail.js
@@ -12,59 +12,67 @@ const ContainerDetail = ({
 }) => {
     return (
         <Table>
-            <Table.Row> 
-                <Table.Cell>Image</Table.Cell>
-                <Table.Cell>{image}</Table.Cell>
-            </Table.Row>
-            <Table.Row> 
-                <Table.Cell>Command</Table.Cell>
-                <Table.Cell>{command}</Table.Cell>
-            </Table.Row>
-            <Table.Row> 
-                <Table.Cell>ENV</Table.Cell>
-                <Table.Cell>
-                    <Table>
-                        {
-                            env.map( (v,i) => (
+            <Table.Body>
+                <Table.Row> 
+                    <Table.Cell>Image</Table.Cell>
+                    <Table.Cell>{image}</Table.Cell>
+                </Table.Row>
+                <Table.Row> 
+                    <Table.Cell>Command</Table.Cell>
+                    <Table.Cell>{command}</Table.Cell>
+                </Table.Row>
+                <Table.Row> 
+                    <Table.Cell>ENV</Table.Cell>
+                    <Table.Cell>
+                        <Table>
+                            <Table.Body>
+                                {
+                                    env.map( (v,i) => (
+                                        <Table.Row key={i}>
+                                            <Table.Cell>{v.split('=')[0]}</Table.Cell>                               
+                                            <Table.Cell>{v.split('=')[1]}</Table.Cell>                               
+                                        </Table.Row>
+                                    ))
+                                }
+                            </Table.Body>
+                        </Table>
+                    </Table.Cell>
+                </Table.Row>
+                <Table.Row> 
+                    <Table.Cell>Labels</Table.Cell>
+                    <Table.Cell>
+                        <Table>
+                            <Table.Body>
+                                {
+                                    keys(labels).map( k => (
+                                        <Table.Row key={k}>
+                                            <Table.Cell>{k}</Table.Cell>
+                                            <Table.Cell>{labels[k]}</Table.Cell>
+                                        </Table.Row>
+                                    ))
+                                }
+                            </Table.Body>
+                        </Table>
+                    </Table.Cell>
+                </Table.Row>
+                <Table.Row> 
+                    <Table.Cell>RestartPolicy</Table.Cell>
+                    <Table.Cell>
+                        <Table>
+                            <Table.Body>
                                 <Table.Row>
-                                    <Table.Cell>{v.split('=')[0]}</Table.Cell>                               
-                                    <Table.Cell>{v.split('=')[1]}</Table.Cell>                               
+                                    <Table.Cell>Name</Table.Cell>
+                                    <Table.Cell>{restartPolicy}</Table.Cell>
                                 </Table.Row>
-                            ))
-                        }
-                    </Table>
-                </Table.Cell>
-            </Table.Row>
-            <Table.Row> 
-                <Table.Cell>Labels</Table.Cell>
-                <Table.Cell>
-                    <Table>
-                        {
-                            keys(labels).map( k => (
                                 <Table.Row>
-                                    <Table.Cell>{k}</Table.Cell>
-                                    <Table.Cell>{labels[k]}</Table.Cell>
+                                    <Table.Cell>maxRetryCount</Table.Cell>
+                                    <Table.Cell>{maxRetryCount}</Table.Cell>
                                 </Table.Row>
-                            ))
-                        }
-                    </Table>
-                </Table.Cell>
-            </Table.Row>
-            <Table.Row> 
-                <Table.Cell>RestartPolicy</Table.Cell>
-                <Table.Cell>
-                    <Table>
-                        <Table.Row>
-                            <Table.Cell>Name</Table.Cell>
-                            <Table.Cell>{restartPolicy}</Table.Cell>
-                        </Table.Row>
-                        <Table.Row>
-                            <Table.Cell>maxRetryCount</Table.Cell>
-                            <Table.Cell>{maxRetryCount}</Table.Cell>
-                        </Table.Row>
-                    </Table>
-                </Table.Cell>
-            </Table.Row>
+                            </Table.Body>
+                        </Table>
+                    </Table.Cell>
+                </Table.Row>
+            </Table.Body>
         </Table>
     );
 }
