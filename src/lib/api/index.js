9b16c38ffcaa1795ca0a3bfaff9dae54655b0352
y0c <wonder789@naver.com>
2018-08-09 22:37:39 +0900

    feat(image): image api & reducer added

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -1,9 +1,11 @@
 import * as AuthApi from './auth';
 import * as UserApi from './user';
 import * as EndpointApi from './endpoint';
+import * as ImageApi from './image';
 
 export {
     AuthApi,
     UserApi,
-    EndpointApi
+    EndpointApi,
+    ImageApi
 };
