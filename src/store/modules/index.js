c85baa604b97c05db473772a24ec5cf607a61b91
HoSung Lim <holnet1026@gmail.com>
2018-08-09 22:37:39 +0900

    feat(image): image api & reducer added

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -5,12 +5,14 @@ import auth from './auth';
 import user from './user';
 import endPoint from './endpoint';
 import container from './container';
+import image from './image';
 
 export default combineReducers({
     auth,
     user,
     endPoint,
     container,
+    image,
     form: formReducer,
     pender: penderReducer
 });
