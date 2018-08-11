47c599ace951387d62a2891bd30cea4e2e88ea77
HoSung Lim <holnet1026@gmail.com>
2018-08-11 23:44:02 +0900

    feat(ui): common component section header added

--- src/components/base/ui/index.js
+++ src/components/base/ui/index.js
@@ -3,11 +3,13 @@ import Spinner from './Spinner';
 import TableNoData from './TableNoData';
 import TableColGroup from './TableColGroup';
 import LinkTitle from './LinkTitle';
+import SectionHeader from './SectionHeader';
 
 export {
     Logo,
     Spinner,
     TableNoData,
     TableColGroup,
-    LinkTitle
+    LinkTitle,
+    SectionHeader
 }
