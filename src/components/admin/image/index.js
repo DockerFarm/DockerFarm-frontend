a9b84a628600ff2bf99378813817d5be88480111
HoSung Lim <holnet1026@gmail.com>
2018-08-12 23:51:06 +0900

    feat(image): image search form added (no-implement)

--- src/components/admin/image/index.js
+++ src/components/admin/image/index.js
@@ -2,11 +2,13 @@ import ImageList from './ImageList';
 import ImageInfo from './ImageInfo';
 import ImageDetail from './ImageDetail';
 import ImageHistory from './ImageHistory';
+import ImageSearchForm from './ImageSearchForm';
 
 
 export {
     ImageList,
     ImageInfo,
     ImageDetail,
-    ImageHistory
+    ImageHistory,
+    ImageSearchForm
 }
