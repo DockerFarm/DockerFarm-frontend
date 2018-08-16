b8076c897884984fb6d006707ac5f15ee3e30853
HoSung Lim <holnet1026@gmail.com>
2018-08-16 22:31:44 +0900

    fix(ci): docker file & jenkins file change

---
+++ src/containers/routes/admin/network/NetworkNewPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Field } from 'redux-form/immutable';
+import { Form } from 'semantic-ui-react';
+import { Aux } from 'components/hoc';
+
+class NetworkNewPage extends Component {
+
+
+    render() {
+        return (
+            <Aux>
+
+            </Aux>
+        )
+    }
+}
+
+export default NetworkNewPage;
