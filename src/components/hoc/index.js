1eb01d4b6d66208bea559679a83b44ab0d2c0376
Hosung Lim <holnet1026@gmail.com>
2018-08-25 01:01:48 +0900

    feat(hoc): condition rendering hoc If added

--- src/components/hoc/index.js
+++ src/components/hoc/index.js
@@ -1,10 +1,12 @@
 import withAuth from './withAuth';
+import If from './If';
 import Aux from './Aux';
 import withErrorHandler from './withErrorHandler'
 import withSocketProgress from './withSocketProgress';
 
 export {
     withAuth,
+    If,
     Aux,
     withErrorHandler,
     withSocketProgress
