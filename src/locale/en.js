0fce6056865e53c9249be8e46c55303fb4089739
Hosung Lim <holnet1026@gmail.com>
2018-08-29 23:47:28 +0900

    feat(i18n): lang file added

---
+++ src/locale/en.js
@@ -0,0 +1,83 @@
+export default {
+
+    // Side bar menu
+    'MENU_DASHBOARD': 'Dashboard',
+    'MENU_CONTAINER': 'Containers',
+    'MENU_IMAGE': 'Images',
+    'MENU_NETWORK': 'Networks',
+    'MENU_VOLUME': 'Volumes',
+    'MENU_LIBRARY': 'Library',
+    'MENU_SETTING': 'Settings',
+    'MENU_REGISTRY': 'Registries',
+
+    //GNB 
+    'GNB_LOGOUT': 'Logout',
+
+    //DashBoard Page
+    'DB_CONTAINER_TITLE': 'Containers',
+    'DB_CONTAINER_RUNNING': 'running',
+    'DB_CONTAINER_STOPPED': 'stopped',
+    'DB_IMAGE_TITLE': 'Images',
+    'DB_IMAGE_TOTAL': 'Total',
+    'DB_NETWORK_TITLE': 'Networks',
+    'DB_VOLUME_TITLE': 'Volumes',
+    'DB_NODE_HEADER': 'Node Info',
+    'DB_NODE_NAME': 'Name',
+    'DB_NODE_VERSION': 'Docker Version',
+    'DB_NODE_CPU': 'Node CPU',
+    'DB_NODE_MEMORY': 'Node Memory',
+
+    //Container Management
+    'CON_LIST_HEADER': 'Container List',
+    'CON_INFO_HEADER': 'Container Info',
+    'CON_DETAIL_HEADER': 'Container Details',
+    'CON_VOLUME_HEADER': 'Container in Volume',
+
+
+    //Image Management
+    'IMG_LIST_HEADER': 'Image List',
+    'IMG_SEARCH_HEADER': 'Search Image',
+    'IMG_INFO_HEADER': 'Image Info',
+    'IMG_DETAIL_HEADER': 'Image Details',
+    'IMG_HISTORY_HEADER': 'Image History',
+    'IMG_BUILD_HEADER': 'Build Image',
+    'IMG_BTN_BUILD': 'Build a New Image',
+    'IMG_BTN_BUILD_SAVE': 'Build',
+
+
+    //Network Management
+    'NET_LIST_HEADER': 'Network List',
+    'NET_CREATE_HEADER': 'Network Create',
+    'NET_INFO_HEADER': 'Network Info',
+    'NET_OPTION_HEADER': 'Network Options',
+    'NET_CONTAINER_HEADER': 'Network using Container',
+    'NET_BTN_ADD': 'Add Network',
+    'NET_BTN_DELETE': 'Delete Network',
+    'NET_BTN_LEAVE': 'Leave Network',
+
+
+    //Volume Management
+    'VOL_LIST_HEADER': 'Volume List',
+    'VOL_INFO_HEADER': 'Volume Info',
+    'VOL_CREATE_HEADER': 'Volume Create',
+    'VOL_CONTAINER_HEADER': 'Volume using Container',
+    'VOL_DETAIL_HEADER': 'Volume Details',
+    'VOL_BTN_ADD': 'Add Volume',
+
+    //Library
+    'LIB_DESCRIPTION_HEADER': 'You can look around the Offial Image and get a Pull!',
+
+    //REGISTRY
+    'REG_LIST_HEADER': 'Registry List',
+    'REG_INFO_HEADER': 'Registry Info',
+    'REG_BTN_ADD': 'Add Registry',
+
+    //Common
+    'BTN_REMOVE': 'Delete',
+    'BTN_SAVE': 'Save',
+    'BTN_LIST': 'List',
+    'BTN_RESET': 'Reset',
+    'BTN_GARBEGE_COLLECTION': 'Grabege Collection',
+
+    'TABLE_NUMBER_OF_PAGES': 'Number of Pages'
+}
