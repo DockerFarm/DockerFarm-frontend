31d17f27bda8acf9f9aae8619918dd1f01160429
Hosung Lim <holnet1026@gmail.com>
2018-08-29 23:48:21 +0900

    feat(i18n): setup react-intl redux

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { IntlProvider } from 'react-intl-redux';
+import { addLocaleData } from 'react-intl';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from 'containers/App';
 import store from 'store';
+import en from 'react-intl/locale-data/en'
+import ko from 'react-intl/locale-data/ko'
+import ja from 'react-intl/locale-data/ja'
 
 
+addLocaleData([...en, ...ko, ...ja]);
 
 const Root = () => (
     <Provider store={store}>
-        <Router>
-            <App/>
-        </Router> 
+        <IntlProvider>
+            <Router>
+                <App/>
+            </Router> 
+        </IntlProvider>
     </Provider>
 )
 
